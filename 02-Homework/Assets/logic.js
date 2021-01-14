39db514bee21ad9e4794912e81fc0caf02564dff
Gustaf Gonzalez <tony.campoy@gmail.com>
2021-01-14 17:11:04 -0600

    fixed input form

--- 02-Homework/Assets/logic.js
+++ 02-Homework/Assets/logic.js
@@ -1,36 +1,66 @@
-function getWeather(){
+// function getWeather(){
 
-var city = $("#form1").val();
-var queryURL = "https://cors-anywhere.herokuapp.com/api.openweathermap.org/data/2.5/forecast?q="
-+ city +"&appid=19eb70296734629e1adb73f7e235752b";
+// var city = $("#form1").val();
+// var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q="
+// + city +"&appid=19eb70296734629e1adb73f7e235752b";
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function(response){
-    console.log(response);
+// $.ajax({
+//     url: queryURL,
+//     method: "GET"
+//   }).then(function(response){
+//     console.log(response);
     
-    var currentCity = $("#name");
-    currentCity.text(response.name)
-    $("#name").append(currentCity)
+//     var currentCity = $("#name");
+//     currentCity.text(response.name)
+//     $("#name").append(currentCity)
     
-    var Kalvin = response.main.temp
-    var temp = (Kalvin -273.15) *1.80 +32
-    tempF = Math.floor(temp)
-    $("#temp").text(tempF + " F")
+//     var Kalvin = response.main.temp
+//     var temp = (Kalvin -273.15) *1.80 +32
+//     tempF = Math.floor(temp)
+//     $("#temp").text(tempF + " F")
     
-    var currentHumidity = $("#humidity")
-    currentHumidity.text(response.main.humidity)
-    $("#humidity").append(currentHumidity)
+//     var currentHumidity = $("#humidity")
+//     currentHumidity.text(response.main.humidity)
+//     $("#humidity").append(currentHumidity)
 
-    var currentWindSpeed = $("#wind")
-    currentWindSpeed.text(response.wind.speed)
-    $("#wind").append(currentWindSpeed)
+//     var currentWindSpeed = $("#wind")
+//     currentWindSpeed.text(response.wind.speed)
+//     $("#wind").append(currentWindSpeed)
 
+//   })
+
+// }
+
+$("#search-button").on("click", function(event){
+  event.preventDefault();
+  var city = $("#search-city").val().trim();
+  var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q="
+  + city +"&appid=19eb70296734629e1adb73f7e235752b";
+  
+  $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response){
+      console.log(response);
+      
+      var currentCity = $("#name");
+      currentCity.text(response.name)
+      $("#name").append(currentCity)
+      
+      // var Kalvin = response.main.temp
+      // var temp = (Kalvin -273.15) *1.80 +32
+      // tempF = Math.floor(temp)
+      // $("#temp").text(tempF + " F")
+      
+      // var currentHumidity = $("#humidity")
+      // currentHumidity.text(response.main.humidity)
+      // $("#humidity").append(currentHumidity)
+  
+      // var currentWindSpeed = $("#wind")
+      // currentWindSpeed.text(response.wind.speed)
+      // $("#wind").append(currentWindSpeed)
+  
+    })
+  
   })
 
-}
-
-$("#searchcity").on("click", function(){
-    getWeather();
-})
