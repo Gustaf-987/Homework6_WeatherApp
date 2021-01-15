b127a06c7f582b97f822275c171165a7cda9116a
Gustaf Gonzalez <tony.campoy@gmail.com>
2021-01-14 19:22:12 -0600

    text can now append

--- 02-Homework/Assets/logic.js
+++ 02-Homework/Assets/logic.js
@@ -1,40 +1,11 @@
-// function getWeather(){
-
-// var city = $("#form1").val();
-// var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q="
-// + city +"&appid=19eb70296734629e1adb73f7e235752b";
-
-// $.ajax({
-//     url: queryURL,
-//     method: "GET"
-//   }).then(function(response){
-//     console.log(response);
-    
-//     var currentCity = $("#name");
-//     currentCity.text(response.name)
-//     $("#name").append(currentCity)
-    
-//     var Kalvin = response.main.temp
-//     var temp = (Kalvin -273.15) *1.80 +32
-//     tempF = Math.floor(temp)
-//     $("#temp").text(tempF + " F")
-    
-//     var currentHumidity = $("#humidity")
-//     currentHumidity.text(response.main.humidity)
-//     $("#humidity").append(currentHumidity)
-
-//     var currentWindSpeed = $("#wind")
-//     currentWindSpeed.text(response.wind.speed)
-//     $("#wind").append(currentWindSpeed)
-
-//   })
-
-// }
-
 $("#search-button").on("click", function(event){
   event.preventDefault();
   var city = $("#search-city").val().trim();
-  var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q="
+  // var savedCities = $("#cityList").val();
+  // savedCities.push(city);
+  // localStorage.setItem("#cityList", JSON.stringify(savedCities));
+
+  var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="
   + city +"&appid=19eb70296734629e1adb73f7e235752b";
   
   $.ajax({
@@ -43,22 +14,23 @@ $("#search-button").on("click", function(event){
     }).then(function(response){
       console.log(response);
       
+      
       var currentCity = $("#name");
       currentCity.text(response.name)
       $("#name").append(currentCity)
       
-      // var Kalvin = response.main.temp
-      // var temp = (Kalvin -273.15) *1.80 +32
-      // tempF = Math.floor(temp)
-      // $("#temp").text(tempF + " F")
+      var Kalvin = response.main.temp
+      var temp = (Kalvin -273.15) *1.80 +32
+      tempF = Math.floor(temp)
+      $("#temp").text(tempF + " F")
       
-      // var currentHumidity = $("#humidity")
-      // currentHumidity.text(response.main.humidity)
-      // $("#humidity").append(currentHumidity)
+      var currentHumidity = $("#humidity")
+      currentHumidity.text(response.main.humidity + " %")
+      $("#humidity").append(currentHumidity)
   
-      // var currentWindSpeed = $("#wind")
-      // currentWindSpeed.text(response.wind.speed)
-      // $("#wind").append(currentWindSpeed)
+      var currentWindSpeed = $("#wind")
+      currentWindSpeed.text(response.wind.speed)
+      $("#wind").append(currentWindSpeed)
   
     })
   
