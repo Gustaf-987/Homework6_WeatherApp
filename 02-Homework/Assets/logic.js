f1369c47fdefc6b5ddb437d647279f3581a10a69
Gustaf Gonzalez <tony.campoy@gmail.com>
2021-01-14 21:42:55 -0600

    5 day forecast updates

--- 02-Homework/Assets/logic.js
+++ 02-Homework/Assets/logic.js
@@ -1,4 +1,5 @@
 var cities =[];
+var currentTime = moment()._d.getDate();
 
 $("#search-button").on("click", function(event){
   event.preventDefault();
@@ -39,6 +40,77 @@ $("#search-button").on("click", function(event){
       updateHistory();
   
     })
+
+    var queryURL =  "http://api.openweathermap.org/data/2.5/forecast?q=" + city 
+    + "&appid=19eb70296734629e1adb73f7e235752b";
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function (response){
+      console.log(response);
+
+      var day1Date = $("#day1date")
+      day1Date.text(response.list[2].dt_txt);
+      var day2Date = $("#day2date")
+      day2Date.text(response.list[10].dt_txt);
+      var day3Date = $("#day3date")
+      day3Date.text(response.list[18].dt_txt);
+      var day4Date = $("#day4date")
+      day4Date.text(response.list[26].dt_txt);
+      var day5Date = $("#day5date")
+      day5Date.text(response.list[34].dt_txt);
+
+      var Kalvin = response.list[2].main.temp
+      var temperature = (Kalvin - 273.15) *1.80 + 32
+      tempF = Math.floor(temperature);
+      $("#temperature-day1").text(tempF + "F")
+
+      var Kalvin = response.list[10].main.temp
+      var temperature = (Kalvin - 273.15) *1.80 + 32
+      tempF = Math.floor(temperature);
+      $("#temperature-day2").text(tempF + "F")
+
+      var Kalvin = response.list[18].main.temp
+      var temperature = (Kalvin - 273.15) *1.80 + 32
+      tempF = Math.floor(temperature);
+      $("#temperature-day3").text(tempF + "F")
+
+      var Kalvin = response.list[26].main.temp
+      var temperature = (Kalvin - 273.15) *1.80 + 32
+      tempF = Math.floor(temperature);
+      $("#temperature-day4").text(tempF + "F")
+
+      var Kalvin = response.list[34].main.temp
+      var temperature = (Kalvin - 273.15) *1.80 + 32
+      tempF = Math.floor(temperature);
+      $("#temperature-day5").text(tempF + "F")
+
+      var day1Image = $("#image-day1")
+      var newLink1 = "http://openweathermap.org/img/wn/" + response.list[2].weather[0].icon + "@2x.png"
+      day1Image.attr("src", newLink1)
+
+      var day2Image = $("#image-day2")
+      var newLink1 = "http://openweathermap.org/img/wn/" + response.list[10].weather[0].icon + "@2x.png"
+      day2Image.attr("src", newLink1)
+
+      var day3Image = $("#image-day3")
+      var newLink1 = "http://openweathermap.org/img/wn/" + response.list[18].weather[0].icon + "@2x.png"
+      day2Image.attr("src", newLink1)
+
+      var day4Image = $("#image-day4")
+      var newLink1 = "http://openweathermap.org/img/wn/" + response.list[26].weather[0].icon + "@2x.png"
+      day2Image.attr("src", newLink1)
+
+      var day5Image = $("#image-day5")
+      var newLink1 = "http://openweathermap.org/img/wn/" + response.list[34].weather[0].icon + "@2x.png"
+      day2Image.attr("src", newLink1)
+
+
+    })
+
+
+
+
     function updateHistory() {
       cities = JSON.parse((localStorage.getItem("Cities"))) || []
       for (let i = 1; i < 6; i++) {
